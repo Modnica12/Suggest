1e9633add61ec8c5a0b70fd9cf50fbdf5ebfc959
Modnica12 <jcycu12@gmail.com>
2019-10-04 10:15:22 +0500

    first

---
+++ test_suggest.py
@@ -0,0 +1,24 @@
+import unittest
+from sug import suggest
+
+
+class TestSuggest(unittest.TestCase):
+    # def test_abakanets(self):
+    #    sug = suggest.Suggester("ruwords.txt")
+    #    res = sug.get('аб')
+    #    # print(res)
+    #    self.assertEqual("абаканец\n" in res, True)
+
+    def test_count(self):
+        sug = suggest.Suggester("ruwords.txt")
+        res = sug.get('при')
+
+        self.assertEqual(len(res), suggest.MAX_FOUND)
+
+    def test_t(self):
+        sug = suggest.Suggester("ruwords.txt")
+        res = sug.get('')
+
+        print(res)
+
+        self.assertEqual(len(res), suggest.MAX_FOUND)
